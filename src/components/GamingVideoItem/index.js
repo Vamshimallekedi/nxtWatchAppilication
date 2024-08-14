b7b5d29dac7d6174d01c54361660f02954d02ef8
vamshi <vamshimallekedi1@gmail.com>
2024-08-14 17:50:33 +0000

    nxt watch

---
+++ src/components/GamingVideoItem/index.js
@@ -0,0 +1,28 @@
+import {
+  VideoCardContainer,
+  ThumbnailImage,
+  VideoCardBottomContainer,
+  VideoDetailsContainer,
+  VideoDetailsText,
+  NavLink,
+} from './styledComponents'
+
+const VideoCardTwo = props => {
+  const {details} = props
+  const {title, id, thumbnailUrl, viewCount} = details
+  return (
+    <NavLink to={`videos/${id}`}>
+      <VideoCardContainer>
+        <ThumbnailImage src={thumbnailUrl} alt="video thumbnail" />
+        <VideoCardBottomContainer>
+          <VideoDetailsContainer>
+            <VideoDetailsText>{title}</VideoDetailsText>
+            <VideoDetailsText>{viewCount} views</VideoDetailsText>
+          </VideoDetailsContainer>
+        </VideoCardBottomContainer>
+      </VideoCardContainer>
+    </NavLink>
+  )
+}
+
+export default VideoCardTwo
